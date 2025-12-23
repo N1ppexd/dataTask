d6eafe46ff0507ae4a9447f072e28a6d6f558262
N1ppexd <niilohanni@gmail.com>
2025-12-23 12:26:34 +0200

    sfdsdafsdf

--- dataTask/App.tsx
+++ dataTask/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AstronomyData from './components/astronomyData';
+import AstronomyData from './components/AstronomyData';
 
 export default function App() {
 
